5c6a7e5bb0b75762059fbdd0dd273ef73b887b0e
goldblackk <45857590+goldblackk@users.noreply.github.com>
2020-05-03 12:12:47 +0200

    Add files via upload

---
+++ goldPassword/goldPassword.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+static const char alphanum[] =
+"0123456789"
+"!@#$%^&*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz";
+
+int stringLength = sizeof(alphanum) - 1;
+
+char randomString()
+{
+    return alphanum[rand() % stringLength];
+}
+
+int main()
+{
+    int length;
+    SetConsoleTitle( TEXT("goldPassword - Password Generator by goldblack"));
+    system("Color 0A");
+    cout << "Please, enter password length: ";
+    cin >> length;
+    srand(time(0));
+    string password;
+    for (unsigned int i = 0; i < length; ++i)
+        password += randomString();
+
+    cout << password << endl;
+    system("pause");
+
+    return 0;
+}
